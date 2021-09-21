9f38c9d281ce644dab614de54cc9f79829f24ef2
Sergey Dmitiev <msgsed@gmail.com>
2021-09-21 06:59:31 +0300

    Add news cells

---
+++ vk-app/news/NewsHeaderViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  NewsHeaderViewCell.swift
+//  vk-app
+//
+//  Created by Sergey Dmitriev on 21.09.2021.
+//
+
+import Foundation
---
+++ vk-app/news/NewsViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  NewsViewCell.swift
+//  vk-app
+//
+//  Created by Sergey Dmitriev on 21.09.2021.
+//
+
+import Foundation
